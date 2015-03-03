c1113fa96417bad36bd81a8e611907f4138d4d9e
larsfrommars <larsfrommars@gmail.com>
2015-03-03 15:11:30 +0100

    init

---
+++ src/main/groovy/Main.groovy
@@ -0,0 +1,18 @@
+// (C) 1998-2015 Information Desire Software GmbH
+// www.infodesire.com
+
+class MainGroovy {
+  
+  public static void main( java.lang.String... args ) {
+    new MainGroovy().someLibraryMethod();
+  }
+  
+  boolean someLibraryMethod() {
+    println "Hello World!"
+    true
+  }
+
+}
+
+
+
